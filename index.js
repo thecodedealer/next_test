dc0287fa0022eb8ebb5845027b102ac0c7e7bf53
thecodedealer <37584555+thecodedealer@users.noreply.github.com>
2022-02-15 17:38:23 +0200

    test

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-const spawn = require('child_process').spawn;
-const child = spawn('npm install && npm run build && npm run start', {
-  shell: true
-});
-child.stderr.on('data', function (data) {
-  console.error("STDERR:", data.toString());
-});
-child.stdout.on('data', function (data) {
-  console.log("STDOUT:", data.toString());
-});
-child.on('exit', function (exitCode) {
-  console.log("Child exited with code: " + exitCode);
-});
+console.log('>>>> helo')
