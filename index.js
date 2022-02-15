a6663cb6644a4f64ead28b8f8aeef521d1d3384b
thecodedealer <37584555+thecodedealer@users.noreply.github.com>
2022-02-15 17:21:16 +0200

    fix

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const { parse } = require('url')
 const next = require('next')
 
 const dev = process.env.NODE_ENV !== 'production'
-const hostname = 'localhost'
-const port = 3000
+const hostname = 'brave-beaver.159-89-13-202.plesk.page'
+const port = 80
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port })
 const handle = app.getRequestHandler()
